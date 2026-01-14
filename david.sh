f7bdbd3108ba51ce50ff4cddc481ddaa4b403f6e
DAVID <tbdavid2019@gmail.com>
2026-01-14 09:51:09 +0800

    20260114v

--- david.sh
+++
@@ -1,6 +0,0 @@
-docker build -t stock-index-api .
-
-
-
-#運行容器：
-docker run -d -p 8090:8000 -v $(pwd)/data:/app/data --name stock-api stock-index-api
--- upload2KV.sh
+++ upload2KV.sh
@@ -8,7 +8,7 @@ python3 crawler-i18n.py
 # 需要使用 --path 參數從檔案讀取內容
 # 需要加上 --remote 參數才會上傳到雲端而非本地
 # wrangler login
-pnpm install
+# pnpm install
 
 cd data && \
 pnpm wrangler kv key put SP500 --namespace-id=5e8e4092fd964584a2152c4a6f948d47 --path sp500_data.json --remote && \
